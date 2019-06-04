d07c8436a4758e8498956d734b4c58e643c058b9
awang617 <wanga18@mail.wlu.edu>
2019-06-04 14:26:46 -0700

    more description

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,8 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Home</h1>
+                <h1>The Language of Flowers</h1>
+                <h3>Search for flowers or their meanings!</h3>
             </div>
         )
     }
