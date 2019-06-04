1575a7ba7e5e0a3ade188087e8043d6ec75dee72
awang617 <wanga18@mail.wlu.edu>
2019-06-04 14:28:08 -0700

    added dropdown to search by name or meaning

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react'
 
 class Search extends Component {
+
     render() {
         return (
-            <div>
-                <h1>Search</h1>
-            </div>
+            <form>
+                <select name="search" onChange={this.props.onChange}>
+                    <option value="name">Name</option>
+                    <option value="meaning">Meaning</option>
+                </select>
+                <input placeholder="What are you looking for?" onInput={this.props.onInput}/>
+                <input type="submit" value="Search"/>
+            </form>
         )
     }
 }
