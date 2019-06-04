21741be74e4709aea7543e42308656e7280d8c04
awang617 <wanga18@mail.wlu.edu>
2019-06-03 23:01:07 -0700

    created container component

---
+++ src/SearchContainer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Search from './Search'
+
+class SearchContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Search Container</h1>
+                <Search />
+            </div>
+        )
+    }
+}
+
+export default SearchContainer
