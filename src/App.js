2925953b1167d325f3b112cfcc0fb85ec8a01c90
awang617 <wanga18@mail.wlu.edu>
2019-06-04 21:58:14 -0700

    importing react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from './Home'
 import SearchContainer from './SearchContainer'
+import {
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom'
 
 function App() {
   return (
