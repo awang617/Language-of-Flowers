71e282787a47de115435cdb2124b567dcf74a44e
awang617 <wanga18@mail.wlu.edu>
2019-06-04 23:04:11 -0700

    moved container into Home

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import SearchContainer from './SearchContainer'
 
 class Home extends Component {
     render() {
@@ -6,6 +7,7 @@ class Home extends Component {
             <div>
                 <h1>The Language of Flowers</h1>
                 <h3>Search for flowers or their meanings!</h3>
+                <SearchContainer />
             </div>
         )
     }
